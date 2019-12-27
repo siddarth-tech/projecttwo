4efc4d7d7744e60727b1484feb3967f47d3c5162
siddarth-tech <59189853+siddarth-tech@users.noreply.github.com>
2019-12-28 00:46:12 +0530

    Update add.java

--- add.java
+++ add.java
@@ -1,10 +1,11 @@
-class{
-	public Static void main(String[] args){
-		int a= 20;
-		int b = 30;
-		int c;
-		c = a+b;
-		System.out.println(c);
+public class Test1 {
+	
+		public static void main(String[] args){
+			int a= 20;
+			int b = 30;
+			int c;
+			c = a+b;
+			System.out.println(c);
+		}
 	}
-}
 
